37d6d42e2419efc1dad95acbe45a9787d6e5ff36
etxsA <93368439+etxsA@users.noreply.github.com>
2025-10-09 13:37:33 -0600

    Added DbModule

---
+++ src/db/db.service.ts
@@ -0,0 +1,26 @@
+/*eslint-disable prettier/prettier*/
+
+import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
+import {Pool, createPool} from "mysql2/promise";
+
+@Injectable()
+export class DbService implements OnModuleInit, OnModuleDestroy {
+    private pool: Pool;
+
+    onModuleInit() {
+        this.pool = createPool ({
+            host: 'localhost',
+            user: 'access1',
+            password: 'access1-p',
+            database: 'oFraud'
+        }) 
+    }
+
+    onModuleDestroy() {
+        void this.pool.end();
+    }
+
+    getPool(): Pool {
+        return this.pool;
+    }
+}
