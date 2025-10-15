22adf01ba1bc4130cf78a1d1a7cd4ed2ff5e364c
etxsA <93368439+etxsA@users.noreply.github.com>
2025-10-14 18:57:12 -0600

    Changed access token duration to 15m

--- src/auth/tokens.service.ts
+++ src/auth/tokens.service.ts
@@ -1,4 +1,3 @@
-/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UserModel } from 'src/user/entities/user.model';
@@ -6,68 +5,61 @@ import { AccessPayload, RefreshPayload } from './tokens.interface';
 
 @Injectable()
 export class TokensService {
+  constructor(private readonly jwtService: JwtService) {}
 
-    constructor (private readonly jwtService: JwtService) {}
+  async generateAccessToken(profile: UserModel): Promise<string> {
+    return await this.jwtService.signAsync(
+      {
+        sub: profile.id,
+        type: 'access',
+        profile: {
+          id: profile.id,
+          name: profile.name,
+          email: profile.email,
+          admin: profile.admin,
+        },
+      },
+      {
+        expiresIn: '15m',
+        secret: 'supersecret',
+      },
+    );
+  }
 
-    async generateAccessToken(profile: UserModel): Promise<string> {
-        
-        return await this.jwtService.signAsync(
-            {
-                sub: profile.id,
-                type: "access",
-                profile: {
-                    id: profile.id,
-                    name: profile.name,
-                    email: profile.email,
-                    admin: profile.admin
-                }
-            }, {
-                expiresIn: "1m",
-                secret: "supersecret",
-            }
-        );
+  async generateRefreshToken(profile: UserModel): Promise<string> {
+    return await this.jwtService.signAsync(
+      {
+        sub: profile.id,
+        type: 'refresh',
+      },
+      {
+        expiresIn: '7d',
+        secret: 'supersecret',
+      },
+    );
+  }
 
-        
+  async verifyAccessToken(token: string): Promise<AccessPayload> {
+    const payload: AccessPayload = await this.jwtService.verifyAsync(token, {
+      secret: 'supersecret',
+    });
+
+    if (payload.type !== 'access') {
+      throw new Error('Invalid token type');
     }
 
-    async generateRefreshToken(profile: UserModel): Promise<string> {
-        return await this.jwtService.signAsync(
-            {
-                sub: profile.id,
-                type: "refresh",
-            }, {
-                expiresIn: "7d",
-                secret: "supersecret",
-            }
-        );
+    return payload;
+  }
 
-     
+  async verifyRefeshToken(token: string): Promise<RefreshPayload> {
+    const payload: RefreshPayload = await this.jwtService.verifyAsync(token, {
+      secret: 'supersecret',
+    });
 
+    if (payload.type !== 'refresh') {
+      throw new Error('Invalid token type');
     }
 
-    async verifyAccessToken(token: string): Promise<AccessPayload> {
-
-        const payload: AccessPayload = await this.jwtService.verifyAsync(token, {
-            secret: "supersecret",
-        });
-        
-        if (payload.type !== "access") {
-            throw new Error("Invalid token type");
-        }
-
-        return payload;
-    }
-
-    async verifyRefeshToken(token: string): Promise<RefreshPayload> {
-        const payload: RefreshPayload = await this.jwtService.verifyAsync(token, {
-            secret: "supersecret",
-        });
-
-        if (payload.type !== "refresh") {
-            throw new Error("Invalid token type");
-        }
-
-        return payload;
-    }
-
+    return payload;
+  }
 }
