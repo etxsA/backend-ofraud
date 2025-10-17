8f94c6140d01fc080fd872508e8282156f19d6a5
etxsA <93368439+etxsA@users.noreply.github.com>
2025-10-16 21:54:06 -0600

    Enable cors for local connection with the frontend

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,9 @@ import 'dotenv/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,,PUT,PATCH,POST,DELETE,OPTIONS',});
   const config = new DocumentBuilder()
     .setTitle('API ofraud')
     .setDescription(
